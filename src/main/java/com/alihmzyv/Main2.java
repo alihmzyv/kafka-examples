b8506426711bb589f2e2057ba8bd72cb8701d80f
ali hamzayev <008461@ibar.int>
2025-01-05 13:49:22 +0400

    experiment with broker configs.

--- src/main/java/com/alihmzyv/Main2.java
+++ src/main/java/com/alihmzyv/Main2.java
@@ -2,22 +2,25 @@ package com.alihmzyv;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-//When producer tries to send, it even retries (max.block.ms, max-retry count to be accounted for) when the broker is not available.
-public class Main {
+//when auto.create.topics.enable is set to false, the producer will not create the topic if it does not exist. and fail:
+//Topic test not present in metadata after 60000 ms.
+public class Main2 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties kafkaProps = new Properties();
-        kafkaProps.put("bootstrap.servers", "localhost:8081");
+        kafkaProps.put("bootstrap.servers", "localhost:9092");
         kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaProps.put("value.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
         try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(kafkaProps)) {
             ProducerRecord<String, String> record = new ProducerRecord<>("test", "test data");
             System.out.println("Sending the message");
-            kafkaProducer.send(record).get();
+            RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+            System.out.println("Message size: " + recordMetadata.serializedValueSize());
             System.out.println("Sent the message");
         }
     }
